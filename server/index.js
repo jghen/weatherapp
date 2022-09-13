a832d9263a36fb052bc51e2de6c76081bf025da4
Jan Georg Henriksen <jangeorg85@gmail.com>
2022-09-13 23:23:58 +0200

    yeah..

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.get("/cities", async (req, res) => {
